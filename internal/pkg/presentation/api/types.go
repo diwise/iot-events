9e47e954f620639ea07537687158cd75bc710124
Mikael Nordberg <mikael.nordberg@masarinconsulting.se>
2024-07-17 12:16:45 +0200

    meta + links

--- internal/pkg/presentation/api/types.go
+++ internal/pkg/presentation/api/types.go
@@ -11,7 +11,7 @@ type meta struct {
 	TotalRecords uint64  `json:"totalRecords"`
 	Offset       *uint64 `json:"offset,omitempty"`
 	Limit        *uint64 `json:"limit,omitempty"`
-	Count        uint64  `json:"count"`
+	Count        *uint64 `json:"count,omitempty"`
 }
 
 type links struct {
@@ -31,16 +31,22 @@ type ApiResponse struct {
 func NewApiResponse(r *http.Request, data any, count, total, offset, limit uint64) ApiResponse {
 	meta := &meta{
 		TotalRecords: total,
-		Offset: &offset,
-		Limit: &limit,
-		Count: count,
+	}
+
+	if offset > 0 {
+		meta.Offset = &offset
+	}
+
+	if count != total {
+		meta.Limit = &limit
+		meta.Count = &count
 	}
 
 	links := createLinks(r.URL, meta)
 
 	return ApiResponse{
-		Meta: meta,
-		Data: data,
+		Meta:  meta,
+		Data:  data,
 		Links: links,
 	}
 }
@@ -51,38 +57,47 @@ func (r ApiResponse) Byte() []byte {
 }
 
 func createLinks(u *url.URL, m *meta) *links {
-	if m == nil || m.TotalRecords == 0 {
+	if m == nil || m.TotalRecords == 0 || m.Count == nil || (*m.Count == m.TotalRecords) {
 		return nil
 	}
 
 	query := u.Query()
 
-	newUrl := func(offset uint64) *string {
+	newUrl := func(offset int64) *string {
 		query.Set("offset", strconv.Itoa(int(offset)))
 		u.RawQuery = query.Encode()
 		u_ := u.String()
 		return &u_
 	}
 
-	first := uint64(0)
-	last := ((m.TotalRecords - 1) / *m.Limit) * *m.Limit
-	next := *m.Offset + *m.Limit
-	prev := int64(*m.Offset) - int64(*m.Limit)
+	var offset int64 = 0
+	if m.Offset != nil {
+		offset = int64(*m.Offset)
+	}
+
+	var limit int64 = 10
+	if m.Limit != nil {
+		limit = int64(*m.Limit)
+	}
+
+	first := int64(0)
+	last := (int64(m.TotalRecords-1) / limit) * limit
+	next := offset + limit
+	prev := offset - limit
 
 	links := &links{
-		Self:  newUrl(*m.Offset),
+		Self:  newUrl(offset),
 		First: newUrl(first),
 		Last:  newUrl(last),
 	}
 
-	if next < m.TotalRecords {
+	if next < int64(m.TotalRecords) {
 		links.Next = newUrl(next)
 	}
 
 	if prev >= 0 {
-		links.Prev = newUrl(uint64(prev))
+		links.Prev = newUrl(prev)
 	}
 
 	return links
 }
-
